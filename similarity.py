d300dbde1ec8cbabcc287e7999ff8990317019e6
Amitabh Das <adas59@asu.edu>
2019-09-09 00:18:15 -0700

    Added template

--- similarity.py
+++ similarity.py
@@ -1,9 +1,25 @@
+import timeit
 import moment
 import cv2
 import numpy as np
-from pymongo import MongoClient
+import argparse
 
+from pathlib import Path
+from pymongo import MongoClient
 from dynaconf import settings
+
+from jinja2 import Environment
+from jinja2.loaders import FileSystemLoader
+
+env = Environment(loader=FileSystemLoader('templates'))
+tmpl = env.get_template('op_temp.html')
+
+def prepare_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model', type=str, required=True)
+    parser.add_argument('-k', '--k-nearest', type=int, required=True)
+    parser.add_argument('-i', '--image', type=str, required=True)
+    return parser
 
 def calc_mom_sim(img_path, k):
     client = MongoClient(host=settings.HOST, port=settings.PORT, username=settings.USERNAME, password=settings.PASSWORD)
@@ -14,7 +30,11 @@ def calc_mom_sim(img_path, k):
     k_u = np.array(key_feats["u_moments"], dtype=list)
     k_v = np.array(key_feats["v_moments"], dtype=list)
 
+    times = []
+
     def image_index(rec):
+        s = timeit.default_timer()
+
         y = np.array(rec["y_moments"], dtype=list)
         u = np.array(rec["u_moments"], dtype=list)
         v = np.array(rec["v_moments"], dtype=list)
@@ -38,10 +58,55 @@ def calc_mom_sim(img_path, k):
         d_v = np.sum(tmp, axis=1)
 
         d = d_y + d_u + d_v
-        return (rec["path"], d.mean(),)
+        res = (rec["path"], d.mean(),)
+
+        e = timeit.default_timer()
+        times.append(e-s)
+        return res
 
     data = coll.find()
     d = np.array([image_index(data.next()) for _ in range(0, coll.count_documents({}))], dtype=[('x', object), ('y', float)])
     d.sort(order="y")
 
+    times = np.array(times)
+    print("Took AVG {} for each comparision".format(times.mean()))
+    print("Took Total {} for all comparisions".format(times.sum()))
+
     return d[0:k]
+
+# s = timeit.default_timer()
+# ranks = calc_mom_sim("/home/amitab/Documents/MWDB Project/Phase 0/Data/Hands/Hand_0009720.jpg", 100)
+# e = timeit.default_timer()
+# print("Took {} to calculate".format(e - s))
+
+# s = timeit.default_timer()
+# f = open("op.html", "w")
+# f.write(tmpl.render(ranks=ranks, key="/home/amitab/Documents/MWDB Project/Phase 0/Data/Hands/Hand_0009720.jpg", title="TEST"))
+# e = timeit.default_timer()
+# print("Took {} to write".format(e - s))
+
+if __name__ == "__main__":
+    parser = prepare_parser()
+    args = parser.parse_args()
+
+    img_path = Path(args.image)
+    if not img_path.exists() or not img_path.is_file():
+        raise Exception("Invalid image path.")
+
+    if args.k_nearest == 0:
+        raise Exception("Need k > 0")
+
+    s = timeit.default_timer()
+    ranks = calc_mom_sim(str(img_path), args.k_nearest)
+    e = timeit.default_timer()
+    print("Took {} to calculate".format(e - s))
+
+    op_path = Path(settings.OUTPUT_PATH) / "{}.html".format(img_path.resolve().name)
+    s = timeit.default_timer()
+
+    f = open(op_path, "w")
+    f.write(tmpl.render(ranks=ranks, key=str(img_path), title="TEST"))
+
+    e = timeit.default_timer()
+    print("Took {} to write".format(e - s))
+

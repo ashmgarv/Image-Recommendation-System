c8f493ff2acc686dd27c214349468e1007203bde
Amitabh Das <amitab.das@outlook.com>
2019-10-13 03:48:06 -0700

    Added extra credit for task 1 and add print for vectors (#4)

--- tasks/task1.py
+++ tasks/task1.py
@@ -7,6 +7,7 @@ from pprint import pprint
 import sys
 
 sys.path.append('../')
+import output
 from feature_reduction.utils import get_all_vectors
 from feature_reduction.feature_reduction import reducer
 from feature_reduction.utils import get_term_weight_pairs
@@ -30,4 +31,23 @@ if __name__ == '__main__':
     # reducer automatically maps feature_reduction_technique to the right function
     vectors, eigen_values, latent_vs_old = reducer(
         vectors, args.k_latent_semantics, args.feature_reduction_technique)
+
+    pprint(get_term_weight_pairs(vectors), indent=4)
     pprint(get_term_weight_pairs(latent_vs_old), indent=4)
+
+    # Extra Credit
+    # image path with a vector in the latent semantic space
+    data_z = zip(images, vectors)
+    # image path for each latenet semantic in h
+    feature_z = [(idx, images[np.argmax(np.dot(vectors, i[:args.k_latent_semantics]))]) for idx, i in enumerate(latent_vs_old)]
+
+    output.write_to_file("visualize_data_z.html",
+                         "data-z-{}-{}.html".format(args.model, args.feature_reduction_technique),
+                         data_z=data_z,
+                         title="TEST")
+
+    output.write_to_file("visualize_feat_z.html",
+                         "feat-z-{}-{}.html".format(args.model, args.feature_reduction_technique),
+                         feature_z=feature_z,
+                         title="TEST")
+

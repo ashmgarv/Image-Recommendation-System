deeb9cc257aa19b43ad46a62350676dccc839e69
Yash Vijay <yashvijay_17@yahoo.co.in>
2019-12-01 04:21:54 -0700

    Dt yash (#56)
    * Task 1, Feature reduction - added Nan return, settings file set1, set2
    
    * task5
    
    * task5
    
    * LSH code readiness
    
    * Task5 implemented, requires refinement
    
    * Somewhat working task5, store and visualise output
    
    * Is task5 working?
    
    * changed paths
    
    * Master's settings
    
    * T5 Output
    
    * Delete feature_reduction.py
    
    * Delete task1.py
    
    * restoring
    
    * new line
    
    * Task1 changes
    
    * Latest changes reducer funtion
    
    * fixing k min function
    
    * Task1 Final
    
    * Removing output folder
    
    * Adding Settings file dependency and removing code dependency
    
    * removing task5.html weird code

--- feature_reduction/feature_reduction.py
+++ feature_reduction/feature_reduction.py
@@ -23,7 +23,6 @@ def get_pca(vectors, k, **opts):
     #if opts contains query vector, apply scaler and PCA transformation to the query vector and return
     if opts:
         if 'get_scaler_model' in opts and opts['get_scaler_model']:
-            print("returning just the scaler and frt model")
             return pca_vectors, pca.explained_variance_, pca.components_, std_scaler, pca
 
         query_vector = opts['query_vector']
@@ -46,15 +45,16 @@ def get_svd(vectors, k, **opts):
     """
     std_scaler = StandardScaler()
     scaled_values = std_scaler.fit_transform(vectors)
-
+    
     k = min(k, vectors.shape[0], vectors.shape[1])
+    if k == vectors.shape[1]: k-= 1
+
     svd = TruncatedSVD(n_components=k)
     svd_vectors = svd.fit_transform(scaled_values)
 
     #if opts contains query vector, apply scaler and SVD transformation to the query vector and return
     if opts:
         if 'get_scaler_model' in opts and opts['get_scaler_model']:
-            print("returning just the scaler and frt model")
             return svd_vectors, svd.explained_variance_, svd.components_, std_scaler, svd
 
         query_vector = opts['query_vector']
@@ -78,14 +78,12 @@ def get_lda(vectors, k, **opts):
     min_max_scaler = MinMaxScaler()
     scaled_values = min_max_scaler.fit_transform(vectors)
 
-    k = min(k, vectors.shape[0], vectors.shape[1])
+    # k = min(k, vectors.shape[0], vectors.shape[1])
     lda = LatentDirichletAllocation(n_components=k, verbose=0,learning_method='online',n_jobs=-1)
     lda_vectors = lda.fit_transform(scaled_values)
-
     #if opts contains query vector, apply scaler and PCA transformation to the query vector and return
     if opts:
         if 'get_scaler_model' in opts and opts['get_scaler_model']:
-            print("returning just the scaler and frt model")
             return lda_vectors, None, lda.components_, min_max_scaler, lda
 
         query_vector = opts['query_vector']
@@ -111,14 +109,12 @@ def get_nmf(vectors, k, **opts):
     min_max_scaler = MinMaxScaler()
     scaled_values = min_max_scaler.fit_transform(vectors)
 
-    k = min(k, vectors.shape[0], vectors.shape[1])
+    # k = min(k, vectors.shape[0], vectors.shape[1])
     nmf = NMF(n_components=k, init='random', random_state=0)
     nmf_vectors = nmf.fit_transform(scaled_values)
-
     #if opts contains query vector, apply scaler and PCA transformation to the query vector and return
     if opts:
         if 'get_scaler_model' in opts and opts['get_scaler_model']:
-            print("returning just the scaler and frt model")
             return nmf_vectors, None, nmf.components_, min_max_scaler, nmf
 
         query_vector = opts['query_vector']
--- tasks/task1.py
+++ tasks/task1.py
@@ -1,57 +1,136 @@
 import numpy as np
-from pymongo import MongoClient
-from multiprocessing import Pool
-import argparse
+import pandas as pd
 from dynaconf import settings
-from pprint import pprint
-from pathlib import Path
+
+import argparse
 import sys
-import csv
-
 sys.path.append('../')
-import output
-from utils import get_all_vectors, get_term_weight_pairs
+from utils import get_all_vectors, filter_images
 from feature_reduction.feature_reduction import reducer
+from numpy import dot
+from numpy.linalg import norm
+from output import write_to_file
 
-
+# Initiate the argument parses
 def prepare_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--model', type=str, required=True)
-    parser.add_argument('-k', '--k_latent_semantics', type=int, required=True)
-    parser.add_argument(
-        '-frt', '--feature_reduction_technique', type=str, required=True)
+    parser.add_argument('-k', '--Features', type=int, required=True)
     return parser
 
+# Generating the vectors
+def generate_vec():
 
-if __name__ == '__main__':
+    #getting dorsal vectors and class
+    dorsal_paths = filter_images('dorsal')
+    _, dorsal_vectors = get_all_vectors(model, f={'path': {'$in': dorsal_paths}})
+    
+    #getting palmar vectors and class
+    palmar_paths = filter_images('palmar')
+    _, palmar_vectors = get_all_vectors(model, f={'path': {'$in': palmar_paths}})
+    
+    #getting dorsal vectors and class
+    dorsal_paths = filter_images('dorsal', unlabelled_db=True)
+    test_data_dorsal_paths, u_dorsal_vectors = get_all_vectors(model, f={'path': {'$in': dorsal_paths}}, unlabelled_db=True)
+    dorsal_class = np.array([1] * len(u_dorsal_vectors))
+    
+    #getting palmar vectors and class
+    palmar_paths = filter_images('palmar', unlabelled_db=True)
+    test_data_palmar_paths, u_palmar_vectors = get_all_vectors(model, f={'path': {'$in': palmar_paths}}, unlabelled_db=True)
+    palmar_class = np.array([0] * len(u_palmar_vectors))
+
+    # Stacking all the data together for testing purposes
+    test_data  = np.vstack((u_dorsal_vectors,u_palmar_vectors))
+    
+    # Combine Data Paths of Test Data
+    test_data_paths  = np.concatenate((test_data_dorsal_paths,test_data_palmar_paths))
+    
+
+    # Concatinating Lables of test data to calculate accuracy scores later
+    test_labels = np.concatenate((dorsal_class,palmar_class))
+ 
+    # Return the calculated values
+    return dorsal_vectors, palmar_vectors, test_data, test_labels, test_data_paths
+
+
+if __name__ == "__main__":
+    
+    # Initiate cide to take inputs
     parser = prepare_parser()
     args = parser.parse_args()
+    
+    # Only 1 input to be taken, that is k (latent semantics), number of features to be extracted 
+    k_each = args.Features
+            
+    # On extensive testing, the best feature extraction model was found out to be SIFT
+    model = settings.TASK1_CONFIG.MODEL
 
-    images, data_matrix = get_all_vectors(args.model)
+    # On extensive testing, the best feature reduction technique was founf out to be PCA
+    feature = settings.TASK1_CONFIG.FRT
 
-    # reducer automatically maps feature_reduction_technique to the right function
-    vectors, eigen_values, latent_vs_old = reducer(
-        data_matrix, args.k_latent_semantics, args.feature_reduction_technique)
+    # Generating the vectors for Dorsal Labelled, Palmar labelled and the test vectors
+    # Also fetching the labels of unlabelled images so as to check accuracy later
+    dorsal_vectors, palmar_vectors, test_data, test_labels, test_data_paths = generate_vec()
 
-    file_name = "task1_images_vs_latent_{}_{}_{}.csv".format(args.model, args.feature_reduction_technique, args.k_latent_semantics)
-    get_term_weight_pairs(vectors, file_name)
+    # Applying PCA to Dorsal Images and fetching the 'k' latent semantics 
+    reduced_dorsal_vectors, _, _, _, dorsal_pca = reducer(dorsal_vectors,k_each,feature,get_scaler_model=True)
+    dorsal_variance_ratio = dorsal_pca.explained_variance_ratio_
+    print("Computed ",k_each," Latent Semantics for Dorsal")
 
-    file_name = "task1_latent_vs_features_{}_{}_{}.csv".format(args.model, args.feature_reduction_technique, args.k_latent_semantics)
-    get_term_weight_pairs(latent_vs_old, file_name)
+    # Applying PCA to Palmar Images and fetching the 'k' latent semantics 
+    reduced_palmar_vectors, _, _, _, palmar_pca = reducer(palmar_vectors,k_each,feature,get_scaler_model=True)
+    palmar_variance_ratio = palmar_pca.explained_variance_ratio_
+    print("Computed ",k_each," Latent Semantics for Palmar")
+    
+    # Applying PCA to Test Images and fetching the 'k' latent semantics 
+    reduced_test_data, _, _, _, test_pca = reducer(test_data,k_each,feature,get_scaler_model=True)
+    test_variance_ratio = test_pca.explained_variance_ratio_
 
-    # Extra Credit
-    # image path with a vector in the latent semantic space
-    data_z = zip(images, vectors)
-    # image path for each latenet semantic in h
-    feature_z = [(idx, images[np.argmax(np.dot(data_matrix, i))]) for idx, i in enumerate(latent_vs_old)]
+    # Initiate List that will store the total dorsal dot product scores for each test image   
+    dorsal = []
 
-    output.write_to_file("visualize_data_z.html",
-                         "task1-data-z-{}-{}-{}.html".format(args.model, args.feature_reduction_technique, args.k_latent_semantics),
-                         data_z=data_z,
-                         title="TEST")
+    # Initiate List that will store the total palmar dot product scores for each test image   
+    palmar = []
 
-    output.write_to_file("visualize_feat_z.html",
-                         "task1-feat-z-{}-{}-{}.html".format(args.model, args.feature_reduction_technique, args.k_latent_semantics),
-                         feature_z=feature_z,
-                         title="TEST")
+    # Calculate Cosine similarity with every test image with every dorsal image and store them in 'dorsal' list
+    for row in reduced_test_data:
+        dorsalI = 0
+        row = row * test_variance_ratio
+        for row1 in reduced_dorsal_vectors:
+            row1 = row1 * dorsal_variance_ratio
+            dorsalI = dorsalI + dot(row, row1)/(norm(row)*norm(row1))
+        dorsal.append(dorsalI)
 
+    # Calculate Cosine similarity with every test image with every palmar image and store them in 'palmar' list
+    for row2 in reduced_test_data:
+        row2 = row2 * test_variance_ratio
+        palmarI = 0
+        for row3 in reduced_palmar_vectors:
+            row3 = row3 * palmar_variance_ratio
+            palmarI = palmarI + dot(row2, row3)/(norm(row2)*norm(row3))
+        palmar.append(palmarI)
+
+    # Calculation of accuracy scores
+    p_label = []
+    j=0 
+    for i in range(len(reduced_test_data)):
+        p_label.append(0) if palmar[i] < dorsal[i] else p_label.append(1)
+
+    # Commenting out code that calculates and prints accuracy when we fetch the dorsal/palmar value of images from the DB
+    # We will need this code during report formation so as to calculate and display accracy as per task requirement.
+    # This code is a part of the For loop at line 114 (just above)
+        """
+        if p_label[i]==test_labels[i]:
+            j = j + 1
+    print((j/len(reduced_test_data))*100) 
+    """
+    # Change 1 to Dorsal and 0 to Palmar for Visualization purpose
+    final_list = []
+    final_list = ["Dorsal" if i == 1 else "Palmar" for i in p_label]
+    
+    # Code to visualise the output. Check browser or output folder.
+    write_to_file("task4.html",
+                    "task1-{}.html".format(k_each),
+                    predictions=zip(test_data_paths,final_list),
+                    title="Task1")
+    
+            

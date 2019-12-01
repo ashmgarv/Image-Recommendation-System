088458cda24a903766c742d792ee1988d8aa7163
Ankit Sharma <ankit220694@gmail.com>
2019-12-01 10:34:13 -0700

    Done testing and tweaked the value of parameters like k and frt.

--- tasks/task6_probab.py
+++ tasks/task6_probab.py
@@ -23,41 +23,38 @@ def makeArrayBinary(arr, length, breadth):
                 arr[i][j] = 0
     return arr
 
-#query_img = 'C:\\Users\\ankit\\Documents\\GitHub\\Data\\Hands\\Hand_0000002.jpg'
-#relevant = ['C:\\Users\\ankit\\Documents\\GitHub\\Data\\Hands\\Hand_0000002.jpg','C:\\Users\\ankit\\Documents\\GitHub\\Data\\Hands\\Hand_0000003.jpg','C:\\Users\\ankit\\Documents\\GitHub\\Data\\Hands\\Hand_0000007.jpg','C:\\Users\\ankit\\Documents\\GitHub\\Data\\Hands\\Hand_0000008.jpg','C:\\Users\\ankit\\Documents\\GitHub\\Data\\Hands\\Hand_0000014.jpg','C:\\Users\\ankit\\Documents\\GitHub\\Data\\Hands\\Hand_0000015.jpg','C:\\Users\\ankit\\Documents\\GitHub\\Data\\Hands\\Hand_0000013.jpg','C:\\Users\\ankit\\Documents\\GitHub\\Data\\Hands\\Hand_0001527.jpg']
-#irrelevant = ['C:\\Users\\ankit\\Documents\\GitHub\\Data\\Hands\\Hand_0000080.jpg','C:\\Users\\ankit\\Documents\\GitHub\\Data\\Hands\\Hand_0000393.jpg','C:\\Users\\ankit\\Documents\\GitHub\\Data\\Hands\\Hand_0000387.jpg','C:\\Users\\ankit\\Documents\\GitHub\\Data\\Hands\\Hand_0000426.jpg','C:\\Users\\ankit\\Documents\\GitHub\\Data\\Hands\\Hand_0000389.jpg','C:\\Users\\ankit\\Documents\\GitHub\\Data\\Hands\\Hand_0000374.jpg','C:\\Users\\ankit\\Documents\\GitHub\\Data\\Hands\\Hand_0001282.jpg','C:\\Users\\ankit\\Documents\\GitHub\\Data\\Hands\\Hand_0001870.jpg']
-
-model = 'hog'
-frt = 'svd'
-k = 20
+model = 'moment'
+frt = 'nmf'
+k = 30
 
 def feedback_probab(relevant, irrelevant, t):
-    print(len(relevant))
+    
     img_all, img_all_vec = get_all_vectors(model,f={},master_db=True)
     #f={'path': {'$nin': relevant}}
 
     img_all_vec_red, _, __ = reducer(img_all_vec, k, frt)
     img_all_vec_red = scale(img_all_vec_red, 0, 1)
-
-    img_rel_vec_red=[]
+    
     dict_all_red = {}
     for i in range(len(img_all)):
         name = img_all[i]
         dict_all_red[name] = img_all_vec_red[i]
-        if name in relevant:
-            img_rel_vec_red.append(img_all_vec_red[i])
+
+    img_rel_vec_red=[]
+    for name in relevant:
+        img_rel_vec_red.append(dict_all_red[name])
     img_rel_vec_red = np.array(img_rel_vec_red)
 
     img_all_vec_red = makeArrayBinary(img_all_vec_red, img_all_vec_red.shape[0], img_all_vec_red.shape[1])
     img_rel_vec_red = makeArrayBinary(img_rel_vec_red, img_rel_vec_red.shape[0], img_rel_vec_red.shape[1])
-    print(img_rel_vec_red.shape[0])
-    R = len(relevant)
+    
+    R = img_rel_vec_red.shape[0]
     N = len(img_all)
 
     p_list = []
     for j in range(k):
         r = 0
-        for i in range(img_rel_vec_red.shape[0]):
+        for i in range(R):
             if img_rel_vec_red[i][j] == 1:
                 r+=1
         p_list.append((r+0.5)/(R+1))
@@ -76,7 +73,7 @@ def feedback_probab(relevant, irrelevant, t):
     log_list = []
     for i in range(k):
         num = (p_list[i]*(1-n_list[i]))/(n_list[i]*(1-p_list[i]))
-        if num>=0:
+        if num>0:
             log_list.append(math.log(num,2))
     log_list = np.array(log_list)
 
@@ -91,6 +88,5 @@ def feedback_probab(relevant, irrelevant, t):
     for i in range(t):
         final.append(new_result[i][0])
     
-    print(final)
     return final
 
